717dd09ac271e7c45ab6d062c76157cc41ded6b1
Pete West <peterjwest3@gmail.com>
2020-05-30 13:43:01 +0100

    Add test file to diff

---
+++ test.js
@@ -0,0 +1,38 @@
+// Nest body in conditional
+function foo(a) {
+    const x = 1;
+    return a + x;
+}
+
+// Change one part of line, adding new stuff below it
+const bar = [1,2,3].map((x) => x + 1);
+
+// Completely change line except one character
+zim.gir.doThing(b);
+
+// Move one thing above another
+function first(a, b) {
+    return a + b;
+}
+
+function second(x, y) {
+    return x - y;
+}
+
+// Split long lines into multiple lines
+const looong = [[1,2],[3,4],[5,6]].map((x) => x.map((y) => y + Math.random()));
+
+const longString = "https://github.com/peterjwest/git-diff/blob/master/README.md";
+
+// Split long line, with only whitespace changes
+if (Math.random() + 20 >= Math.random() * 20) { return [1,2,3].map((x) => x + 1); }
+
+// Change one line, adding new stuff above it
+function lonely(a) {
+    return a;
+}
+
+// Replace lines with same number of lines, but unrelated
+const x = 123.456;
+const y = x + 0.789;
+const z = Math.max(x, y);
